574fa92ccf49484ea3efa700af1a918d2f7d82ab
vadim-shalapugin <vadimsalapugin@gmail.com>
2023-01-06 13:33:03 +0500

    crated models/auth for users

---
+++ users/authentication.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+
+import jwt
+from django.conf import settings
+from jwt import DecodeError
+from rest_framework.authentication import BaseAuthentication
+
+from .models import CustomUser
+
+
+class Authentication(BaseAuthentication):
+    def authenticate(self, request):
+        data = self.validate_request(request.headers)
+
+        if not data:
+            return None, None
+
+        return self._get_user(data["user_id"]), None
+
+    @staticmethod
+    def _get_user(user_id):
+        try:
+            user = CustomUser.objects.get(id=user_id)
+        except CustomUser.DoesNotExist:
+            return None
+
+        return user
+
+    @staticmethod
+    def validate_request(headers):
+        authorization = headers.get("Authorization", None)
+
+        if not authorization:
+            return None
+
+        token = headers["Authorization"][7:]
+        decoded_data = Authentication._verify_token(token)
+
+        if not decoded_data:
+            return None
+
+        return decoded_data
+
+    @staticmethod
+    def _verify_token(token):
+        try:
+            decoded_data = jwt.decode(
+                token,
+                settings.SECRET_KEY,
+                algorithm="HS256"
+            )
+        except DecodeError:
+            return None
+
+        exp = decoded_data["exp"]
+
+        if datetime.now().timestamp() > exp:
+            return None
+
+        return decoded_data
--- users/models.py
+++ users/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError("Username field is required")
+
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "username"
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"{self.username}"
